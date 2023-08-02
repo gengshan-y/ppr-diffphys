864bb42d7619476474cb3dea84493705c6ea1cc5
Gengshan Yang <luanyingjian@gmail.com>
2023-08-01 23:51:36 -0400

    update

--- diffphys/vis.py
+++ diffphys/vis.py
@@ -1,5 +1,6 @@
 import os
-os.environ["PYOPENGL_PLATFORM"] = "egl" #opengl seems to only work with TPU
+
+os.environ["PYOPENGL_PLATFORM"] = "egl"  # opengl seems to only work with TPU
 import pyrender
 
 import pdb
@@ -9,28 +10,43 @@ import vedo
 from torch.utils.tensorboard import SummaryWriter
 
 import sys
-sys.path.insert(0, '%s/../../'%os.path.join(os.path.dirname(__file__)))
+
+sys.path.insert(0, "%s/../../" % os.path.join(os.path.dirname(__file__)))
 from diffphys.io import save_vid
+
 
 class Logger:
     def __init__(self, opts):
         # vedo vis
         super(Logger, self).__init__()
-        dis=5
-        self.camera = {'pos':[dis,dis/2,dis],
-                  'focalPoint': [0.5,0.5,0.5],
-                  'viewup': [0,1,0]}
-        self.caption = vedo.Text2D("",c="black")
-        self.floor1=vedo.Plane(pos=(.5,.01,.5),normal=(0,1,0), s=(dis/2,dis/2), c='blue8', alpha=0.8)
-        self.floor2=vedo.Plane(pos=(.5,0,.5),normal=(0,1,0), s=(dis*2,dis*2), c='green8',alpha=0.8)
-    
+        dis = 5
+        self.camera = {
+            "pos": [dis, dis / 2, dis],
+            "focalPoint": [0.5, 0.5, 0.5],
+            "viewup": [0, 1, 0],
+        }
+        self.caption = vedo.Text2D("", c="black")
+        self.floor1 = vedo.Plane(
+            pos=(0.5, 0.01, 0.5),
+            normal=(0, 1, 0),
+            s=(dis / 2, dis / 2),
+            c="blue8",
+            alpha=0.8,
+        )
+        self.floor2 = vedo.Plane(
+            pos=(0.5, 0, 0.5),
+            normal=(0, 1, 0),
+            s=(dis * 2, dis * 2),
+            c="green8",
+            alpha=0.8,
+        )
+
         # save
         logname = "%s-%s" % (opts["seqname"], opts["logname"])
         self.save_dir = os.path.join(opts["logroot"], logname)
-        
+
         # tensorboard vis
         self.log = SummaryWriter(self.save_dir, comment=opts["logname"])
-
 
     def show(self, tag, data):
         """
@@ -42,77 +58,105 @@ class Logger:
         """
         # convert tag
         if isinstance(tag, int):
-            tag='%05d'%tag
+            tag = "%05d" % tag
 
-        # create window 
+        # create window
         n_wdw = 2
         resl = 320
-        if 'vs' in data.keys(): n_wdw += 1
-        if 'as' in data.keys(): n_wdw += 1
-        if 'err' in data.keys(): n_wdw += 1
-        if 'tst' in data.keys(): n_wdw += 1
-        
-        if 'camera' in data.keys(): use_gui = False
-        else: use_gui = True
+        if "vs" in data.keys():
+            n_wdw += 1
+        if "as" in data.keys():
+            n_wdw += 1
+        if "err" in data.keys():
+            n_wdw += 1
+        if "tst" in data.keys():
+            n_wdw += 1
+
+        if "camera" in data.keys():
+            use_gui = False
+        else:
+            use_gui = True
 
         if use_gui:
-            self.plt = vedo.Plotter(shape=(1,n_wdw), size=(n_wdw*resl, resl), bg="white", 
-                sharecam=True, resetcam=False)
-            video1 = vedo.Video("%s/simu-%s.gif"%(self.save_dir, tag), backend='ffmpeg', fps=10)
-            video2 = vedo.Video("%s/simu-%s.mp4"%(self.save_dir, tag), backend='opencv', fps=10)
+            self.plt = vedo.Plotter(
+                shape=(1, n_wdw),
+                size=(n_wdw * resl, resl),
+                bg="white",
+                sharecam=True,
+                resetcam=False,
+            )
+            video1 = vedo.Video(
+                "%s/simu-%s.gif" % (self.save_dir, tag), backend="ffmpeg", fps=10
+            )
+            video2 = vedo.Video(
+                "%s/simu-%s.mp4" % (self.save_dir, tag), backend="opencv", fps=10
+            )
             # find the center x/z location
-            vis_offset = np.stack([i.vertices for i in data['xgt']],0)[0].mean(0)
-            vis_offset[1] = 0; vis_offset = vis_offset[None]
+            vis_offset = np.stack([i.vertices for i in data["xgt"]], 0)[0].mean(0)
+            vis_offset[1] = 0
+            vis_offset = vis_offset[None]
         else:
-            vis_offset = np.asarray([[0,0,0]])
+            vis_offset = np.asarray([[0, 0, 0]])
             self.rendered_imgs = [[] for i in range(n_wdw)]
-            img_size = data['img_size']
-            img_size = (img_size[0] * img_size[2], img_size[1] * img_size[2], img_size[2])
+            img_size = data["img_size"]
+            img_size = (
+                img_size[0] * img_size[2],
+                img_size[1] * img_size[2],
+                img_size[2],
+            )
             self.renderer = pyrender.OffscreenRenderer(img_size[0], img_size[1])
 
         # loop over data
-        n_frm = len(data['xs'])
+        n_frm = len(data["xs"])
         for frame in range(n_frm):
             self.wdw_idx = 0
-            self.caption.text("iter:%s, frame:%04d"%(tag, frame))
+            self.caption.text("iter:%s, frame:%04d" % (tag, frame))
             if use_gui:
                 camera = None
             else:
                 # process the scale
-                rtk = data['camera'][frame]
+                rtk = data["camera"][frame]
                 rtk[3] *= img_size[2]
-                camera = {'rtk': rtk}
-          
+                camera = {"rtk": rtk}
+
             # prepare data
-            xgt_mesh = data['xgt'][frame]
-            x_mesh = data['xs'][frame]
-            
+            xgt_mesh = data["xgt"][frame]
+            x_mesh = data["xs"][frame]
+
             # gt mesh
             mesh = vedo.Mesh([xgt_mesh.vertices - vis_offset, xgt_mesh.faces])
-            self.render_wdw(mesh, val=xgt_mesh.visual.vertex_colors, val_max=255, camera=camera)
-           
-            if 'tst' in data.keys():
-                tst_mesh = data['tst'][frame]
+            self.render_wdw(
+                mesh, val=xgt_mesh.visual.vertex_colors, val_max=255, camera=camera
+            )
+
+            if "tst" in data.keys():
+                tst_mesh = data["tst"][frame]
                 mesh = vedo.Mesh([tst_mesh.vertices - vis_offset, tst_mesh.faces])
-                self.render_wdw(mesh, val=tst_mesh.visual.vertex_colors, val_max=255, camera=camera)
-            
+                self.render_wdw(
+                    mesh, val=tst_mesh.visual.vertex_colors, val_max=255, camera=camera
+                )
+
             # estimated mesh
             mesh = vedo.Mesh([x_mesh.vertices - vis_offset, x_mesh.faces])
-            self.render_wdw(mesh, val=x_mesh.visual.vertex_colors, val_max=255, camera=camera)
+            self.render_wdw(
+                mesh, val=x_mesh.visual.vertex_colors, val_max=255, camera=camera
+            )
 
             # error
-            if 'err' in data.keys():
+            if "err" in data.keys():
                 mesh = vedo.Mesh([x_mesh.vertices - vis_offset, x_mesh.faces])
-                self.render_wdw(mesh, val=data['err'][frame], val_max=0.1, camera=camera)
-           
+                self.render_wdw(
+                    mesh, val=data["err"][frame], val_max=0.1, camera=camera
+                )
+
             # acceleration
-            if 'as' in data.keys(): 
+            if "as" in data.keys():
                 mesh = vedo.Mesh([x_mesh.vertices - vis_offset, x_mesh.faces])
-                self.render_wdw(mesh, val=data['as'][frame], val_max=2, camera=camera)
+                self.render_wdw(mesh, val=data["as"][frame], val_max=2, camera=camera)
             # velocity
-            if 'vs' in data.keys(): 
-                mesh = vedo.Mesh([x_mesh.vertices - vis_offset,x_mesh.faces])
-                self.render_wdw(mesh, val=data['vs'][frame], val_max=0.5, camera=camera)
+            if "vs" in data.keys():
+                mesh = vedo.Mesh([x_mesh.vertices - vis_offset, x_mesh.faces])
+                self.render_wdw(mesh, val=data["vs"][frame], val_max=0.5, camera=camera)
 
             if use_gui:
                 video1.addFrame()
@@ -124,63 +168,88 @@ class Logger:
             self.plt.close()
         else:
             for i in range(n_wdw):
-                save_vid("%s/vid%d-simu-%s"%(self.save_dir, i, tag), 
-                        self.rendered_imgs[i],suffix='.gif',upsample_frame=0)
-                save_vid("%s/vid%d-simu-%s"%(self.save_dir, i, tag), 
-                        self.rendered_imgs[i],suffix='.mp4',upsample_frame=0)
+                save_vid(
+                    "%s/vid%d-simu-%s" % (self.save_dir, i, tag),
+                    self.rendered_imgs[i],
+                    suffix=".gif",
+                    upsample_frame=0,
+                )
+                save_vid(
+                    "%s/vid%d-simu-%s" % (self.save_dir, i, tag),
+                    self.rendered_imgs[i],
+                    suffix=".mp4",
+                    upsample_frame=0,
+                )
             self.renderer.delete()
         # TODO save to gltf (given bones etc.)
 
-    def write_log(self, log_data, step) : 
-        for k,v in log_data.items():
+    def write_log(self, log_data, step):
+        for k, v in log_data.items():
             self.log.add_scalar(k, v, step)
 
     def render_wdw(self, mesh, val=None, val_max=1, camera=None):
         if val is not None:
-            if len(val.shape)==1: val = np.tile(val[:,None],(1,3))
+            if len(val.shape) == 1:
+                val = np.tile(val[:, None], (1, 3))
             val = np.clip(val, -val_max, val_max)
-            mesh.pointdata["RGB"] = ((val+val_max)/val_max/2*255).astype(np.uint8)
+            mesh.pointdata["RGB"] = ((val + val_max) / val_max / 2 * 255).astype(
+                np.uint8
+            )
             mesh.pointdata.select("RGB")
-        if camera is None: 
+        if camera is None:
             camera = self.camera
             self.plt.clear(at=self.wdw_idx)
-            self.plt.show([mesh, self.floor1, self.floor2, self.caption],at=self.wdw_idx, 
-                        resetcam=False, interactive=False,camera=camera)
+            self.plt.show(
+                [mesh, self.floor1, self.floor2, self.caption],
+                at=self.wdw_idx,
+                resetcam=False,
+                interactive=False,
+                camera=camera,
+            )
             self.wdw_idx += 1
         else:
-            img = render_extra(self.renderer, mesh, camera['rtk'])
-            self.rendered_imgs[self.wdw_idx].append( img )
+            img = render_extra(self.renderer, mesh, camera["rtk"])
+            self.rendered_imgs[self.wdw_idx].append(img)
             self.wdw_idx += 1
 
 
 def render_extra(renderer, mesh, camera):
-    mesh = trimesh.Trimesh(mesh.points(), mesh.faces(), vertex_colors=mesh.pointdata["RGB"])
-    plane_transform = np.eye(4); plane_transform[1,1] = -1
+    mesh = trimesh.Trimesh(
+        mesh.points(), mesh.faces(), vertex_colors=mesh.pointdata["RGB"]
+    )
+    plane_transform = np.eye(4)
+    plane_transform[1, 1] = -1
     floor = trimesh.primitives.Box(extents=[10, 0, 10], transform=plane_transform)
-    floor.visual.vertex_colors[:,:3] = 192
+    floor.visual.vertex_colors[:, :3] = 192
     mesh = trimesh.util.concatenate([mesh, floor])
 
-    mesh.vertices = mesh.vertices @ camera[:3,:3].T + camera[:3,3][None]
-    scene = pyrender.Scene(ambient_light=0.4*np.asarray([1.,1.,1.,1.]))
-    meshr = pyrender.Mesh.from_trimesh(mesh,smooth=False)
-    meshr._primitives[0].material.RoughnessFactor=.5
-    scene.add_node( pyrender.Node(mesh=meshr ))
-    
+    mesh.vertices = mesh.vertices @ camera[:3, :3].T + camera[:3, 3][None]
+    scene = pyrender.Scene(ambient_light=0.4 * np.asarray([1.0, 1.0, 1.0, 1.0]))
+    meshr = pyrender.Mesh.from_trimesh(mesh, smooth=False)
+    meshr._primitives[0].material.RoughnessFactor = 0.5
+    scene.add_node(pyrender.Node(mesh=meshr))
+
     cam = pyrender.IntrinsicsCamera(
-            camera[3,0],
-            camera[3,1],
-            camera[3,2],
-            camera[3,3],
-            znear=1e-3,zfar=1000)
-    cam_pose = -np.eye(4); cam_pose[0,0]=1; cam_pose[-1,-1]=1
+        camera[3, 0], camera[3, 1], camera[3, 2], camera[3, 3], znear=1e-3, zfar=1000
+    )
+    cam_pose = -np.eye(4)
+    cam_pose[0, 0] = 1
+    cam_pose[-1, -1] = 1
     scene.add(cam, pose=cam_pose)
 
     direc_l = pyrender.DirectionalLight(color=np.ones(3), intensity=2.0)
-    theta = 9*np.pi/9
-    light_pose = np.asarray([[1,0,0,0],[0,np.cos(theta),-np.sin(theta),0],[0,np.sin(theta),np.cos(theta),0],[0,0,0,1]])
+    theta = 9 * np.pi / 9
+    light_pose = np.asarray(
+        [
+            [1, 0, 0, 0],
+            [0, np.cos(theta), -np.sin(theta), 0],
+            [0, np.sin(theta), np.cos(theta), 0],
+            [0, 0, 0, 1],
+        ]
+    )
     direc_l_node = scene.add(direc_l, pose=light_pose)
 
-    color, _ = renderer.render(scene,flags=pyrender.RenderFlags.SHADOWS_DIRECTIONAL)
-    #color, _ = renderer.render(scene,flags=pyrender.RenderFlags.SHADOWS_DIRECTIONAL | pyrender.RenderFlags.SKIP_CULL_FACES)
-    #cv2.imwrite('tmp/0.jpg', color)
+    color, _ = renderer.render(scene, flags=pyrender.RenderFlags.SHADOWS_DIRECTIONAL)
+    # color, _ = renderer.render(scene,flags=pyrender.RenderFlags.SHADOWS_DIRECTIONAL | pyrender.RenderFlags.SKIP_CULL_FACES)
+    # cv2.imwrite('tmp/0.jpg', color)
     return color
--- diffphys/warp_env.py
+++ diffphys/warp_env.py
@@ -191,51 +191,34 @@ def bullet2gl(msm, in_bullet):
     msm["avel"] = (issac_to_gl @ msm["avel"][..., None])[..., 0]
 
 
-def pred_est_q(steps_fr, nerf_root_rts, nerf_body_rts, bg_rts):
+def pred_est_q(steps_fr, obj_field, bg_field):
     """
     bs,T
     robot2world = scale(bg2world @ bg2view^-1 @ root2view @ se3)
     """
     bs, n_fr = steps_fr.shape
-    device = steps_fr.device
-    data_offset = bg_rts.time_embedding.frame_offset
+    data_offset = bg_field.camera_mlp.time_embedding.frame_offset
     vidid, _ = fid_reindex(steps_fr, len(data_offset) - 1, data_offset)
     vidid = vidid.long()
 
-    # canonical to view
-    from lab4d.utils.quat_transform import quaternion_translation_to_se3
-
-    quat_trans = nerf_root_rts.get_vals(steps_fr.reshape(-1).long())
-    pred_mat = quaternion_translation_to_se3(quat_trans[0], quat_trans[1])
-
-    ##TODO further rotate along x axis
-    # rot_offset = cv2.Rodrigues(np.asarray([0.2,0.,0.]))[0]
-    # rot_offset = torch.Tensor(rot_offset).to(device)[None]
-    # pred_mat[:,:3,:3] = rot_offset @ pred_mat[:,:3,:3]
-
-    # intermediate outputs
-    obj2view = pred_mat.clone().view(bs, n_fr, 4, 4)
-
-    # background transform
-    zero_ones = torch.zeros_like(pred_mat[:, :1])
-    zero_ones[..., -1] = 1
-    quat_trans = bg_rts.get_vals(steps_fr.reshape(-1).long())  # -1,3,4
-    bgrt = quaternion_translation_to_se3(quat_trans[0], quat_trans[1])
-
-    pred_mat = bgrt.inverse() @ pred_mat
-    # bg2world = se3_vec2mat(bg_rts.bg2world[vidid.view(-1)])
-    # bg2world[..., :3, 3] *= bg_rts.bg2fg_scale[vidid.view(-1)][..., None].exp()
-    # pred_mat = bg2world @ pred_mat
+    obj_to_view = obj_field.get_camera(frame_id=steps_fr.reshape(-1).long())
+    scene_to_view = bg_field.get_camera(frame_id=steps_fr.reshape(-1).long())  # -1,3,4
+    obj_to_scene = scene_to_view.inverse() @ obj_to_view
+    # scene_to_world = bg_field.get_rectirication_se3()
+    # obj_to_world = scene_to_world @ obj_to_scene
+    obj_to_world = obj_to_scene
 
     # cv to gl coords
-    cv2gl = torch.eye(4).to(pred_mat.device)
+    cv2gl = torch.eye(4).to(obj_to_world.device)
     cv2gl[1, 1] = -1
     cv2gl[2, 2] = -1
-    pred_mat = cv2gl[None] @ pred_mat
+    obj_to_world = cv2gl[None] @ obj_to_world
 
-    pred_q = se3_mat2vec(pred_mat)  # xyzw
-    pred_q = pred_q.view(bs, n_fr, -1)
-    return pred_q, obj2view
+    obj_to_world_vec = se3_mat2vec(obj_to_world)  # xyzw
+    obj_to_world_vec = obj_to_world_vec.view(bs, n_fr, -1)
+
+    obj_to_view = obj_to_view.clone().view(bs, n_fr, 4, 4)
+    return obj_to_world_vec, obj_to_view
 
 
 def pred_est_ja(steps_fr, nerf_body_rts, env, robot):
@@ -276,15 +259,15 @@ class WarpRootMLP(nn.Module):
         # self.bg_mlp.ft_bgcam=True # asssuming it's accurate enough
 
     def forward(self, x):
-        out, _ = pred_est_q(x, self.root_mlp, self.body_mlp, self.bg_mlp)
+        out, _ = pred_est_q(x, self.root_mlp, self.bg_mlp)
         return out
 
-    def override_states(self, banmo_root_mlp, banmo_body_mlp, banmo_bg_mlp):
+    def override_states(self, banmo_root_mlp, banmo_bg_mlp):
         self.root_mlp.load_state_dict(banmo_root_mlp.state_dict())
         # self.body_mlp.load_state_dict(banmo_body_mlp.state_dict())
         self.bg_mlp.load_state_dict(banmo_bg_mlp.state_dict())
 
-    def override_states_inv(self, banmo_root_mlp, banmo_body_mlp, banmo_bg_mlp):
+    def override_states_inv(self, banmo_root_mlp, banmo_bg_mlp):
         banmo_root_mlp.load_state_dict(self.root_mlp.state_dict())
         # banmo_body_mlp.load_state_dict(self.body_mlp.state_dict())
         banmo_bg_mlp.load_state_dict(self.bg_mlp.state_dict())
@@ -542,9 +525,9 @@ class phys_model(nn.Module):
 
         if self.use_dr:
             # TODO create new modules
-            self.delta_joint_est_mlp = WarpBodyMLP(self.nerf_body_rts)
+            self.delta_joint_est_mlp = WarpBodyMLP(self.obj_field.warp.articulation)
             self.delta_root_mlp = WarpRootMLP(
-                self.nerf_root_rts, self.delta_joint_est_mlp, self.bg_rts
+                self.obj_field, self.delta_joint_est_mlp, self.bg_field
             )
             # self.delta_joint_ref_mlp = WarpBodyMLP(self.nerf_body_rts)
 
@@ -556,13 +539,13 @@ class phys_model(nn.Module):
         self.th_multip = 0  # seems to cause problems
 
     def pred_est_q(self, steps_fr):
-        out, obj2view = pred_est_q(
-            steps_fr, self.nerf_root_rts, self.nerf_body_rts, self.bg_rts
-        )
+        out, obj2view = pred_est_q(steps_fr, self.obj_field, self.bg_field)
         return out, obj2view
 
     def pred_est_ja(self, steps_fr):
-        out = pred_est_ja(steps_fr, self.nerf_body_rts, self.env, self.robot)
+        out = pred_est_ja(
+            steps_fr, self.obj_field.warp.articulation, self.env, self.robot
+        )
         return out
 
     def warmup_state_estimate(self):
@@ -651,12 +634,11 @@ class phys_model(nn.Module):
             setattr(self, state_name, self.state_steps)
 
     def preset_data_dr(self, model_dict):
-        self.bg_rts = model_dict["bg_rts"]
-        self.nerf_root_rts = model_dict["nerf_root_rts"]
-        self.nerf_body_rts = model_dict["nerf_body_rts"]
-        self.ks_params = model_dict["ks_params"]
+        self.bg_field = model_dict["bg_field"]
+        self.obj_field = model_dict["obj_field"]
+        self.intrinsics = model_dict["intrinsics"]
 
-        self.data_offset = self.nerf_body_rts.time_embedding.frame_offset
+        self.data_offset = self.bg_field.camera_mlp.time_embedding.frame_offset
         self.samp_int = 0.1
         self.gt_steps = self.data_offset[-1] - 1
         self.gt_steps_visible = self.gt_steps
@@ -804,7 +786,7 @@ class phys_model(nn.Module):
         # batch['target_jad']= self.compute_gradient(self.pred_est_ja, steps_fr.clone()) / self.samp_int
         batch["target_qd"] = torch.zeros_like(batch["target_q"])[..., :6]
         batch["target_jad"] = torch.zeros_like(batch["target_ja"])
-        batch["ks"] = self.ks_params.get_vals(steps_fr.reshape(-1).long()).reshape(
+        batch["ks"] = self.intrinsics.get_vals(steps_fr.reshape(-1).long()).reshape(
             bs, n_fr, -1
         )
         return batch
@@ -930,17 +912,13 @@ class phys_model(nn.Module):
         return ref, state_q, state_qd, torques, res_f
 
     def override_states(self):
-        self.delta_root_mlp.override_states(
-            self.nerf_root_rts, self.nerf_body_rts, self.bg_rts
-        )
-        self.delta_joint_est_mlp.override_states(self.nerf_body_rts)
+        self.delta_root_mlp.override_states(self.obj_field, self.bg_field)
+        self.delta_joint_est_mlp.override_states(self.obj_field.warp.articulation)
         # self.delta_joint_ref_mlp.override_states(self.nerf_body_rts)
 
     def override_states_inv(self):
-        self.delta_root_mlp.override_states_inv(
-            self.nerf_root_rts, self.nerf_body_rts, self.bg_rts
-        )
-        self.delta_joint_est_mlp.override_states_inv(self.nerf_body_rts)
+        self.delta_root_mlp.override_states_inv(self.obj_field, self.bg_field)
+        self.delta_joint_est_mlp.override_states_inv(self.obj_field.warp.articulation)
 
     def forward(self, frame_start=None):
         # capture requires cuda memory to be pre-allocated
@@ -1319,7 +1297,6 @@ class phys_model(nn.Module):
             # get cameras: world to view = world to object + object to view
             # this triggers pyrender
             obj2world = self.target_q_vis[0]
-            device = obj2world.device
             obj2world = se3_vec2mat(obj2world)
             world2obj = obj2world.inverse()
 
